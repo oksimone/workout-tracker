144f2d190daec2d21ef30eef05e3fbcef9b96a55
ksimone <ksworthy@windstream.net>
2020-09-23 22:58:56 -0400

    deploying

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
+
 const PORT = process.env.PORT || 3000;
 
 
@@ -14,7 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/frozen-beach-01445", { useNewUrlParser: true,
+  useUnifiedTopology:true,
+  useCreateIndex:true,
+  useFindAndModify:false
+ }
+ );
 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes"));
